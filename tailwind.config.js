0d03bc7812a56b9de3cb9283cf0f05a80310981d
berkanbasaran05 <77754197+berkanbasaran05@users.noreply.github.com>
2023-01-17 02:17:08 +0300

    h

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,tsx,jsx,tsx}"
+    "./components/**/*.{js,ts,tsx,jsx,tsx}"
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
