e02c0f388d3ecbc328479230ffa1494a718d6975
berkanbasaran05 <77754197+berkanbasaran05@users.noreply.github.com>
2023-01-23 00:06:25 +0300

    last

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -8,8 +8,8 @@ function ExperienceCard({}: Props) {
     <article
     
     className='flex flex-col rounded-3xl bg-opacity-5  items-center
-     space-y-7 flex-shrink-0 w-[400px]  md:w-[400px] md:h-[700px] xl:w-[500px] xl:h-[700px]
-      snap-center h-[600px] p-10 hover:opacity-70 cursor-pointer transition-opacity duration-200  bg-white'>
+     space-y-7 flex-shrink-0 w-[300px]  md:w-[400px] md:h-[700px] xl:w-[300px] xl:h-[500px]
+      snap-center h-[500px] p-10 hover:opacity-70 cursor-pointer transition-opacity duration-200  bg-white'>
     <motion.img
     initial={{
         y:-100,
@@ -19,14 +19,14 @@ function ExperienceCard({}: Props) {
         duration: 1.2}}
     whileInView={{opacity:1, y:0}}
     
-    className='w-32 h-32 mt-4 rounded-full xl:w-[200px] xl:h-[200px] object-cover
+    className='w-32 h-32  rounded-full xl:w-[96px] xl:h-[96px] object-cover
      object-center'  
     src='https://www.linkpicture.com/q/1673025977937.jpeg'
     alt=''
     />
     <div className='px-0 md:px-10'>
-      <h4 className='text-white text-2xl uppercase font-light'>Ceo of SolWARRIOR</h4>
-      <p className='font-bold text-2xl mt-1 text-white opacity-25'>SOLWARRIOR </p> 
+      <h4 className='text-white text-sm uppercase font-light'>Ceo of SolWARRIOR</h4>
+      <p className='font-bold text-m mt-1 text-white  opacity-25'>SOLWARRIOR </p> 
       <div className='flex space-x-2 my-2'> 
        <img 
        className='h-10 w-10 rounded-full'
@@ -42,9 +42,9 @@ function ExperienceCard({}: Props) {
        src='https://pbs.twimg.com/profile_images/1456412742961799168/p1uuQ0_7_400x400.jpg'
        />
      </div>  
-     <p className='uppercase py-5 text-gray-300'>Started work ... -Ended..</p>
+     <p className='uppercase  text-gray-300 text-sm'>Started work ... -Ended..</p>
 
-       <ul className='list-disc space-y-4 ml-5 text-sm text-white opacity-25'>
+       <ul className='list-disc space-y-4 mt-2 ml-5 text-[8px] xl:text-sm text-white opacity-25'>
         <li>hey hey hey hey hey heyhey hey hey</li>
         <li>hey hey hey hey hey heyhey hey hey</li>
         <li>hey hey hey hey hey heyhey hey hey</li>
